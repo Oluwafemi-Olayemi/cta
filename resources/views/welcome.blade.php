4a9cd5604170a5b41b5390bbe9d686ad10c71235
Oluwafemi Olayemi <3621041+Oluwafemi-Olayemi@users.noreply.github.com>
2023-06-15 20:46:30 +0100

    added grant criteria

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -91,7 +91,6 @@
         <section id="intro">
             <div class="intro-container wow fadeIn">
                 <h1 class="mb-4 pb-0" style="color: #f82249">Conference<br>travel Application</h1>
-{{--                <p class="mb-4 pb-0">10-12 December, Downtown Conference Center, New York</p>--}}
                 <a href="#" class="venobox play-btn mb-4" data-vbtype="video"
                    data-autoplay="true"></a>
                 <a href="#about" class="about-btn scrollto">About Us</a>
